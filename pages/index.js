4fe84f43296f1f1cb63ee57e4ca81ee4a4f56ea2
maczyt <369280416@qq.com>
2017-11-25 10:01:48 +0800

    init repo

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+import Hello from '../components/Hello'
+
+export default () => (
+  <div>
+    Welcome to next.js!
+    <Hello />
+  </div>
+)
